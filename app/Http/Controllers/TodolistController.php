36474b71bce7522d17b918118b9fa9df092c3c74
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-04-25 14:05:33 +0700

    Membuat Halaman Todo

--- app/Http/Controllers/TodolistController.php
+++ app/Http/Controllers/TodolistController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TodolistService;
 use Illuminate\Http\Request;
 
 class TodolistController extends Controller
 {
 
+    private TodolistService $todolistService;
+
+    public function __construct(TodolistService $todolistService)
+    {
+        $this->todolistService = $todolistService;
+    }
+
     public function todoList(Request $request)
     {
-
+        $todolist = $this->todolistService->getTodolist();
+        return response()->view("todolist.todolist", [
+            "title" => "Todolist",
+            "todolist" => $todolist
+        ]);
     }
 
     public function addTodo(Request $request)
---
+++ tests/Feature/TodolistControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodolistControllerTest extends TestCase
+{
+    public function testTodolist()
+    {
+        $this->withSession([
+            "user" => "khannedy",
+            "todolist" => [
+                [
+                    "id" => "1",
+                    "todo" => "Eko"
+                ],
+                [
+                    "id" => "2",
+                    "todo" => "Kurniawan"
+                ]
+            ]
+        ])->get('/todolist')
+            ->assertSeeText("1")
+            ->assertSeeText("Eko")
+            ->assertSeeText("2")
+            ->assertSeeText("Kurniawan");
+    }
+}
