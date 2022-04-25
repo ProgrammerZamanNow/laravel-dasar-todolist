b904b34c320324d4e5f5f324c4278b5ebc19af89
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-04-25 13:56:27 +0700

    Membuat Todolist Controller :broccoli:

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,10 @@ Route::controller(\App\Http\Controllers\UserController::class)->group(function (
     Route::post('/login', 'doLogin')->middleware([\App\Http\Middleware\OnlyGuestMiddleware::class]);
     Route::post('/logout', 'doLogout')->middleware([\App\Http\Middleware\OnlyMemberMiddleware::class]);
 });
+
+Route::controller(\App\Http\Controllers\TodolistController::class)
+    ->middleware([\App\Http\Middleware\OnlyMemberMiddleware::class])->group(function () {
+        Route::get('/todolist', 'todoList');
+        Route::post('/todolist', 'addTodo');
+        Route::post('/todolist/{id}/delete', 'removeTodo');
+    });
