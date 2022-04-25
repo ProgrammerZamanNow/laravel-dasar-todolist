9c542c7e57bdef4821f1d4e0ddb52ed5b06d2aa1
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-04-25 13:56:31 +0700

    Membuat Todolist Controller :broccoli:

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+
+    public function todoList(Request $request)
+    {
+
+    }
+
+    public function addTodo(Request $request)
+    {
+
+    }
+
+    public function removeTodo(Request $request, string $todoId)
+    {
+
+    }
+
+}
