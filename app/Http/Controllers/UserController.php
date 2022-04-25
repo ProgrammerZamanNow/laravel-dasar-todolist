8e243fcb16867cd1dc8963f857734a4fe3f896d3
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-04-25 12:43:24 +0700

    Login Action

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UserService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
+    private UserService $userService;
+
+    /**
+     * @param UserService $userService
+     */
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
     public function login(): Response
     {
         return response()
@@ -15,9 +27,28 @@ class UserController extends Controller
             ]);
     }
 
-    public function doLogin()
+    public function doLogin(Request $request): Response|RedirectResponse
     {
+        $user = $request->input('user');
+        $password = $request->input('password');
 
+        // validate input
+        if (empty($user) || empty($password)) {
+            return response()->view("user.login", [
+                "title" => "Login",
+                "error" => "User or password is required"
+            ]);
+        }
+
+        if($this->userService->login($user, $password)){
+            $request->session()->put("user", $user);
+            return redirect("/");
+        }
+
+        return response()->view("user.login", [
+            "title" => "Login",
+            "error" => "User or password is wrong"
+        ]);
     }
 
     public function doLogout()
--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -14,4 +14,27 @@ class UserControllerTest extends TestCase
             ->assertSeeText("Login");
     }
 
+    public function testLoginSuccess()
+    {
+        $this->post('/login', [
+            "user" => "khannedy",
+            "password" => "rahasia"
+        ])->assertRedirect("/")
+            ->assertSessionHas("user", "khannedy");
+    }
+
+    public function testLoginValidationError()
+    {
+        $this->post("/login", [])
+            ->assertSeeText("User or password is required");
+    }
+
+    public function testLoginFailed()
+    {
+        $this->post('/login', [
+            'user' => "wrong",
+            "password" => "wrong"
+        ])->assertSeeText("User or password is wrong");
+    }
+
 }
